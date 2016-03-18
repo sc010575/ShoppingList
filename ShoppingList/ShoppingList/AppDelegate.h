d40d1e1fec2035cd8781e2374c8d00f132889bc3
Suman Chatterjee <chatterjeesuman@gmail.com>
2016-03-18 10:04:05 +0000

    Project setup

---
+++ ShoppingList/ShoppingList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingList
+//
+//  Created by Suman Chatterjee on 18/03/2016.
+//  Copyright © 2016 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingList/ShoppingList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingList
+//
+//  Created by Suman Chatterjee on 18/03/2016.
+//  Copyright © 2016 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
